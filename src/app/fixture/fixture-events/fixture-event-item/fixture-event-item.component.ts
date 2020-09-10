08a3de355adfbe890d80daf804a0faabea36637a
sesan07 <31746240+sesan07@users.noreply.github.com>
2020-09-09 21:41:44 -0300

    Improved fixture events, added icons

--- src/app/fixture/fixture-events/fixture-event-item/fixture-event-item.component.ts
+++ src/app/fixture/fixture-events/fixture-event-item/fixture-event-item.component.ts
@@ -10,11 +10,47 @@ export class FixtureEventItemComponent implements OnInit {
   @Input() fixtureEvent: FixtureEvent;
   @Input() isHomeTeam: boolean;
 
+  // isGoal: boolean;
+  isNormalGoal: boolean;
+  isOwnGoal: boolean;
+  isPenalty: boolean;
+  isMissedPenalty: boolean;
+
+  isCard: boolean;
+  isYellowCard: boolean;
+  isRedCard: boolean;
+  isSubstitution: boolean;
+
+  playerOne: string = null;
+  playerTwo: string = null;
+
   // TODO hasStarted, hasFinished
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.fixtureEvent.type === 'Goal') {
+      // this.isGoal = true;
+      this.isNormalGoal = this.fixtureEvent.detail === 'Normal Goal';
+      this.isOwnGoal = this.fixtureEvent.detail === 'Own Goal';
+      this.isPenalty = this.fixtureEvent.detail === 'Penalty';
+      this.isMissedPenalty = this.fixtureEvent.detail === 'Missed Penalty';
+
+      this.playerOne = this.fixtureEvent.player;
+      this.playerTwo = this.fixtureEvent.assist;
+    }
+    else if (this.fixtureEvent.type === 'subst') {
+      this.isSubstitution = true;
+
+      this.playerOne = this.fixtureEvent.player;
+      this.playerTwo = this.fixtureEvent.detail;
+    }
+    else if (this.fixtureEvent.type === 'Card') {
+      this.isYellowCard = this.fixtureEvent.detail === 'Yellow Card';
+      this.isRedCard = this.fixtureEvent.detail === 'Red Card';
+
+      this.playerOne = this.fixtureEvent.player;
+    }
   }
 
 }
