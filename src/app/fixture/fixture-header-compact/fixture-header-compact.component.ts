3de9d00927a80d8c4cf6d6ddc9d2e74e267ecaca
sesan07 <31746240+sesan07@users.noreply.github.com>
2020-09-18 14:32:40 -0300

    Fixture view has like button

--- src/app/fixture/fixture-header-compact/fixture-header-compact.component.ts
+++ src/app/fixture/fixture-header-compact/fixture-header-compact.component.ts
@@ -1,12 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Fixture} from '../../shared/models/fixture.model';
+import {Subscription} from 'rxjs';
+import {FavoritesService} from '../../shared/services/favorites.service';
 
 @Component({
   selector: 'app-fixture-header-compact',
   templateUrl: './fixture-header-compact.component.html',
   styleUrls: ['./fixture-header-compact.component.scss']
 })
-export class FixtureHeaderCompactComponent implements OnInit {
+export class FixtureHeaderCompactComponent implements OnInit, OnDestroy {
   @Input() fixture: Fixture;
 
   showScore: boolean;
@@ -14,7 +16,10 @@ export class FixtureHeaderCompactComponent implements OnInit {
   showElapsed: boolean;
   showStatus: boolean;
 
-  constructor() { }
+  isFavorite: boolean;
+  favoritesSub: Subscription;
+
+  constructor(private favoritesService: FavoritesService) { }
 
   ngOnInit(): void {
     const statusCode = this.fixture.statusShort;
@@ -23,6 +28,25 @@ export class FixtureHeaderCompactComponent implements OnInit {
     this.showVs = !this.showScore;
     this.showElapsed = this.fixture.elapsed && statusCode !== 'FT';
     this.showStatus = statusCode !== 'NS';
+
+    this.isFavorite = this.favoritesService.isFavoriteFixture(this.fixture.fixture_id);
+    this.favoritesSub = this.favoritesService.favoriteFixtureRemoved.subscribe((fixtureId: number) => {
+      if (fixtureId === this.fixture.fixture_id) { this.isFavorite = false; }
+    });
+  }
+
+  onFavouriteClicked() {
+    if (this.isFavorite) {
+      this.isFavorite = false;
+      this.favoritesService.removeFixture(this.fixture.fixture_id);
+    } else {
+      this.isFavorite = true;
+      this.favoritesService.addFixture(this.fixture);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.favoritesSub.unsubscribe();
   }
 
 }
--- src/app/fixture/fixture-header/fixture-header.component.ts
+++ src/app/fixture/fixture-header/fixture-header.component.ts
@@ -1,12 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Fixture} from '../../shared/models/fixture.model';
+import {FavoritesService} from '../../shared/services/favorites.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-fixture-header',
   templateUrl: './fixture-header.component.html',
   styleUrls: ['./fixture-header.component.scss']
 })
-export class FixtureHeaderComponent implements OnInit {
+export class FixtureHeaderComponent implements OnInit, OnDestroy {
   @Input() fixture: Fixture;
 
   showScore: boolean;
@@ -14,7 +16,10 @@ export class FixtureHeaderComponent implements OnInit {
   showElapsed: boolean;
   showStatus: boolean;
 
-  constructor() { }
+  isFavorite: boolean;
+  favoritesSub: Subscription;
+
+  constructor(private favoritesService: FavoritesService) { }
 
   ngOnInit(): void {
     const statusCode = this.fixture.statusShort;
@@ -23,6 +28,25 @@ export class FixtureHeaderComponent implements OnInit {
     this.showVs = !this.showScore;
     this.showElapsed = this.fixture.elapsed && statusCode !== 'FT';
     this.showStatus = statusCode !== 'NS';
+
+    this.isFavorite = this.favoritesService.isFavoriteFixture(this.fixture.fixture_id);
+    this.favoritesSub = this.favoritesService.favoriteFixtureRemoved.subscribe((fixtureId: number) => {
+      if (fixtureId === this.fixture.fixture_id) { this.isFavorite = false; }
+    });
+  }
+
+  onFavouriteClicked() {
+    if (this.isFavorite) {
+      this.isFavorite = false;
+      this.favoritesService.removeFixture(this.fixture.fixture_id);
+    } else {
+      this.isFavorite = true;
+      this.favoritesService.addFixture(this.fixture);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.favoritesSub.unsubscribe();
   }
 
 }
