c1f0f51688d9f80869d85c9eece66e8cf705f925
Samuel Esan <samuel.esan@proton.me>
2023-05-19 00:54:49 -0300

    refactor: rename project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/football-site'),
+      dir: require('path').join(__dirname, './coverage/footy-chan'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
